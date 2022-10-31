4d4e71339916b91b98222f44e17e14abc1acb8a1
Hacker-C <1507559148@qq.com>
2022-10-31 11:29:39 +0800

    🐛 fix: footer text color

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-  <footer class="mt-4">
+  <footer class="mt-4 text-color">
     &copy;MurphyChen 2022
     |
     <a 
       href="https://github.com/hacker-c/mphy.me" 
       target="_blank"
-      class="a-link"
+      class="a-link text-color"
     >
       github.com/hacker-c/mphy.me
     </a>
