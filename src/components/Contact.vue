6c013e778cab1753265f741ff998e3edbc6e9e40
Hacker-C <1507559148@qq.com>
2022-11-12 23:59:24 +0800

    ✨ feat: finish social module

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,29 +1,58 @@
 <script setup lang="ts">
 
-import ItemsShow from './common/ItemsShow.vue'
+import SocialItem from './common/SocialItem.vue'
 
 const contacts = [
-  { icon: 'akar-icons:github-fill', text: 'Hacker-C', link: 'https://github.com/Hacker-C' },
+  { 
+    icon: 'akar-icons:github-fill', 
+    text: 'Hacker-C',
+    link: 'https://github.com/Hacker-C',
+    color: 'hover:bg-[#374151]' },
   {
-    icon: 'fa6-brands:square-twitter',
+    icon: 'akar-icons:twitter-fill',
     text: 'MurphyChen',
-    link: 'https://twitter.com/MurphyChen233'
+    link: 'https://twitter.com/MurphyChen233',
+    color: 'hover:bg-[#00acee]'
   },
-  { icon: 'ri:qq-line', text: '1507559148', link: '' },
-  { icon: 'ri:wechat-2-line', text: 'CG1507559148', link: '' },
+  {
+    icon: 'bx:book-bookmark',
+    text: 'Blog',
+    link: 'https://blog.mphy.top',
+    color: 'hover:bg-[#3681e6]',
+    desc: 'Blog'
+  },
+  {
+    icon: 'fa6-solid:book-skull',
+    text: 'IDK-JS',
+    link: 'https://idk-js.mphy.top/',
+    color: 'hover:bg-[#009048]',
+    desc: 'IDK-JS'
+  },
   {
     icon: 'ant-design:zhihu-circle-filled',
     text: 'MurphyChen',
-    link: 'https://www.zhihu.com/people/mphyc'
+    link: 'https://www.zhihu.com/people/mphyc',
+    color: 'hover:bg-[#056de8]'
+  },
+  {
+    icon: 'ri:bilibili-fill',
+    text: 'MurphyChen__',
+    link: 'https://space.bilibili.com/434947088',
+    color: 'hover:bg-[#fb7299]'
+  },
+  { 
+    icon: 'ri:qq-line', 
+    text: '1507559148', 
+    color: 'hover:bg-[#3abcff]'
+  },
+  { 
+    icon: 'ri:wechat-2-line', 
+    text: 'CG1507559148', 
+    color: 'hover:bg-[#07c160]'
   }
-  // { icon: 'fluent:book-question-mark-24-filled', text: 'IDKJS', link: 'http://idk-js.mphy.top' },
-  // { icon: 'bx:book-bookmark', text: 'Notebook', link: 'http://docs.mphy.top' },
-  // { icon: 'ion:terminal', text: 'Terminal', link: 'http://term.mphy.me' }
 ]
 </script>
 
 <template>
-  <ItemsShow :items="contacts">
-    <template v-slot>Contacts</template>
-  </ItemsShow>
+  <SocialItem :items="contacts" />
 </template>
---
+++ src/components/common/SocialItem.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import isNight from '@/utils/theme'
+
+let theme = $computed(() =>
+  isNight.value ? 'color-gray hover-color-white' : 'color-gray-500 hover-color-black'
+)
+
+let style = $computed(() => {
+  return {
+    color: isNight.value ? 'white' : 'black'
+  }
+})
+
+defineProps<{
+  items: {
+    text: string,
+    icon: string,
+    link?: string,
+    color?: string
+    desc?: string
+  }[]
+}>()
+
+let currentIndex = $ref(-1)
+const toggle = (index: number) => {
+  if (currentIndex === index) {
+    currentIndex = -1
+  } else {
+    currentIndex = index
+  }
+}
+</script>
+
+<template>
+  <ul class="flex flex-wrap list-none mt-5">
+    <li
+      v-for="({text, icon, link, color, desc}, index) of items"
+      :key="icon"
+      class="font-[Cute] mr-2 mt-2 h-10 relative"
+    >
+      <a 
+        class="block px-3 py-2 bg-[#f9fafb] decoration-none flex justify-center items-center rounded-md cursor-pointer text-gray-7 hover:text-white"
+        :class="color"
+        :href="link"
+        target="_blank"
+        @click="toggle(index)"
+      >
+        <Icon
+          :icon="icon"
+          :width="icon.includes('skull') ? 19 : 22"
+        />
+        <span v-if="desc" class="ml-1 text-sm">{{ desc }}</span>
+      </a>
+      <template v-if="!link">
+        <div v-show="index === currentIndex" class="text-sm mt-1 w-1 text-gray-text">
+          {{text}}
+        </div>
+      </template>
+     
+    </li>
+  </ul>
+</template>
