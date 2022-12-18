3241fed86e17eef972e8f903eaf127cec508e9bb
Hacker-C <1507559148@qq.com>
2022-12-18 18:26:57 +0800

    ✨ feat: add tags for project item

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -7,38 +7,43 @@ const projects = [
     title: 'Mini Terminal',
     description: 'A mini terminal with simple commands by Vue3',
     link: 'https://term.mphy.me',
-    logo: 'twemoji:laptop'
+    logo: 'twemoji:laptop',
+    tags: ['vue', 'tailwindcss']
   },
   {
     title: 'React 60s View',
     description: '基于 React + Deno 的每日 60 秒读懂世界在线应用',
     link: 'https://60s-view.netlify.app',
-    logo: 'twemoji:world-map'
+    logo: 'twemoji:world-map',
+    tags: ['react', 'deno']
   },
   {
     title: '100 React Apps',
     description: 'Try to finish 100 apps with React',
     link: 'https://100a.netlify.app',
-    logo: 'twemoji:rocket'
+    logo: 'twemoji:rocket',
+    tags: ['react', 'unocss']
   },
   {
     title: 'Can You Google',
     description: 'A toy app to tell you how to google',
     link: 'https://google.mphy.me',
-    logo: 'twemoji:thinking-face'
-
+    logo: 'twemoji:thinking-face',
+    tags: ['vue', 'scss']
   },
   {
     title: 'My Personal Site',
     description: 'A personal site built by vue3',
     link: 'https://github.com/Hacker-C/mphy.me',
-    logo: 'twemoji:man-technologist-medium-skin-tone'
+    logo: 'twemoji:man-technologist-medium-skin-tone',
+    tags: ['vue', 'unocss']
   },
   {
     title: 'WZRY Nodejs Spider',
     description: '使用 Nodejs 爬取王者荣耀英雄技能信息',
     link: 'http://wzry.mphy.top',
-    logo: 'twemoji:spider-web'
+    logo: 'twemoji:spider-web',
+    tags: ['nodejs']
   }
 ]
 
@@ -60,6 +65,7 @@ const projects = [
         :description="item.description"
         :link="item.link"
         :logo="item.logo"
+        :tags="item.tags"
       />
     </div>
   </div>
--- src/components/common/ProjectCard.vue
+++ src/components/common/ProjectCard.vue
@@ -6,17 +6,23 @@ defineProps<{
   description: string
   logo: string
   link: string
+  tags: string[]
 }>()
 </script>
 
 <template>
   <a 
-    class="block rounded-md social-item-bg-color px-4 py-3 my-4 flex font-[Cute] cursor-pointer decoration-none hover:op-90 transition-colors decoration-none" 
+    class="block rounded-md social-item-bg-color px-4 py-3 my-4 flex font-[Cute] cursor-pointer decoration-none op-90 transition-colors decoration-none ease-linear duration-200" 
     :href="link"
     target="_blank"
   >
     <div>
-      <h3 class="title-color text-md">{{ title }}</h3>
+      <div class="flex items-center flex-wrap">
+        <h3 class="title-color text-md mr-2">{{ title }}</h3>
+        <div class="h-8 flex items-center">
+          <span class="border-1 rounded text-sm h-4 leading-4 px-0.5 mr-2 title-color" v-for="tag of tags">{{ tag }}</span>
+        </div>
+      </div>
       <p class="text-color text-sm mt-2">{{ description }}</p>
     </div>
     <span class="flex-1"></span>
