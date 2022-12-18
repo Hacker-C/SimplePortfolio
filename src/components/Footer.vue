0226c13a8644e213cef8f86fe09275fb7b094b26
Hacker-C <1507559148@qq.com>
2022-12-18 18:26:30 +0800

    💄 ui: update page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,19 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
+
 <template>
-  <footer class="mt-5 text-color">
-    &copy;MurphyChen 2022
-    |
+  <footer class="mt-5 text-color flex items-center">
+    <a href="https://mphy.me" class="a-link">
+      &copy;MurphyChen
+    </a>
+    <span class="mx-2">|</span>
     <a 
       href="https://github.com/hacker-c/mphy.me" 
       target="_blank"
       class="a-link text-color"
     >
-      github.com/hacker-c/mphy.me
+      Source
     </a>
   </footer>
 </template>
