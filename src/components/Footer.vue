8c81f4754c4f6007db41543befe9a8ef8b431dcb
Hacker-C <1507559148@qq.com>
2023-01-25 23:07:48 +0800

    💄 ui: Update footer info

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,14 +4,12 @@ import { Icon } from '@iconify/vue';
 
 <template>
   <footer class="mt-5 flex items-center font-[Robot]">
-    <a href="https://mphy.me" class="a-link text-color">
-      &copy;MurphyChen
-    </a>
+    <span class="text-color">MurphyChen &copy; 2022</span>
     <span class="mx-2 text-color">|</span>
     <a 
       href="https://github.com/hacker-c/mphy.me" 
       target="_blank"
-      class="a-link text-color"
+      class="text-color decoration-none"
     >
       Source
     </a>
