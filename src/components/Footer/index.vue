8248aa61cfed1fbd6508b57e00d77f8cfa62feb5
Hacker-C <1507559148@qq.com>
2023-03-27 16:24:58 +0800

    💄 ui: Update footer

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -4,7 +4,7 @@ import meta from '@/config/config'
 
 <template>
   <footer class="mt-5 flex items-center font-[Robot]">
-    <span class="text-color">&copy; 2022-now {{ meta.profile.name }}</span>
+    <span class="text-color">&copy; 2022-PRESENT {{ meta.profile.name }}</span>
     <span class="mx-2 text-color">|</span>
     <a :href="`${meta.source}`" target="_blank" class="text-color decoration-none">
       Source
